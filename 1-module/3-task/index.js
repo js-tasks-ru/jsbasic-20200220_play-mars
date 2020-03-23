7713e5174a16f1868da39a10a56ec852eecadd0b
mars092 <mars0092@gmail.com>
2020-03-24 00:32:01 +0700

    done 1-module/3-task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -4,4 +4,17 @@
  * @returns {string}
  */
 function ucFirst(str) {
+  let new_str = "";
+  let counter = 0;
+
+  for (const char of str) {
+    if (counter == 0) {
+      new_str = char[0].toUpperCase();
+    } else {
+      new_str += char;
+    }
+    counter++;
+  }
+
+  return new_str;
 }
