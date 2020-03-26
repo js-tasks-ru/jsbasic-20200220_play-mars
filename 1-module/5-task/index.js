ff7a61e1716c0efc7272727347b402c93bf406d5
mars092 <mars0092@gmail.com>
2020-03-26 23:54:16 +0700

    done 1-module\4-task and 1-module\5-task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -5,5 +5,17 @@
  * @returns {string}
  */
 function truncate(str, maxlength) {
-
+  if (str.length > maxlength) {
+    let i = 0;
+    let newStr = "";
+    for (const char of str) {
+      if (++i == maxlength) {
+        newStr += "…";
+        return newStr;
+      }
+      newStr += char;
+    }
+  } else {
+    return str;
+  }
 }
