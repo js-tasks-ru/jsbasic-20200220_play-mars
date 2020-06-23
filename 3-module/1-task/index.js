e6eda36c35a3c02ea193bda8c2fb604e7baf94f1
mars092 <mars0092@gmail.com>
2020-06-23 12:33:15 +0700

    3-module\1-task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -4,5 +4,17 @@
  * @param {number} age - максимальный возраст
  * @returns {string}
  */
+
 function showSalary(data, age) {
+  let listUsers = "";
+  let i = 0;
+  data.forEach((element) => {
+    if (element.age <= age && i == 0) {
+      listUsers += `${element.name}: ${element.balance}`;
+    } else if (element.age <= age && i != 0) {
+      listUsers += `\n${element.name}: ${element.balance}`;
+    }
+    i++;
+  });
+  return listUsers;
 }
